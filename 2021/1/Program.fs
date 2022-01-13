b069f49c83ad0e03bf6af5a06be7493c5b0c4153
Łukasz Mrozek <morskk@gmail.com>
2022-01-13 20:06:02 +0100

    First day of AoC 2021

---
+++ 2021/1/Program.fs
@@ -0,0 +1,17 @@
+﻿let firstPart input =
+    input
+    |> Seq.pairwise
+    |> Seq.where (fun (a, b) -> a < b)
+    |> Seq.length
+
+let secondPart input =
+    input
+    |> Seq.windowed 3
+    |> Seq.map (fun x -> Seq.sum x)
+    |> firstPart
+
+let input =
+    System.IO.File.ReadLines("input.txt")
+    |> Seq.map (fun x -> int x)
+
+printfn "%d" (secondPart input)
