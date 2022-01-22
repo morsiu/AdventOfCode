0d5b44b25de57e515dae287e1f55759e5435973b
Łukasz Mrozek <morskk@gmail.com>
2022-01-22 23:32:25 +0100

    Sixth day of AoC 2021, part one

---
+++ 2021/6/Program.fs
@@ -0,0 +1,49 @@
+﻿module Simulation =
+    type Simulation = private Simulation of int * int []
+
+    let create (population : int []) = Simulation(0, population[ 0..8 ])
+    let count (Simulation(_, population)) = Seq.sum population
+    let private getPop age (Simulation(day, population)) =
+        Array.get population ((day + age) % 9)
+    let private setPop age count (Simulation(day, population)) =
+        Array.set population ((day + age) % 9) count
+
+    let private addPop age count simulation =
+        let existingCount = getPop age simulation
+        setPop age (existingCount + count) simulation
+
+    let private takePop age simulation =
+        let count = getPop age simulation
+        setPop age 0 simulation
+        count
+
+    let private nextDay (Simulation(day, population)) =
+        Simulation((day + 1) % 9, population)
+
+    let simulate (simulation : Simulation) =
+        let newFish = takePop 0 simulation
+        let simulation' = nextDay simulation
+        setPop 8 newFish simulation'
+        addPop 6 newFish simulation'
+        simulation'
+
+let input =
+    let population = Array.zeroCreate 9
+    (System.IO.File.ReadLines "input.txt")
+    |> Seq.map (fun x -> x.Split ',')
+    |> Seq.concat
+    |> Seq.map int
+    |> Seq.iter (fun age ->
+           let count = Array.get population age
+           Array.set population age (count + 1))
+    |> ignore
+    Simulation.create population
+
+let firstPart (simulation : Simulation.Simulation) =
+    let result =
+        Seq.replicate 80 0
+        |> Seq.fold (fun simulation _ -> Simulation.simulate simulation)
+               simulation
+    Simulation.count result
+
+printfn "%d" (firstPart input)
