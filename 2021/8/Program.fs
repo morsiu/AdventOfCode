0419b405e5b13f15e88a9e915f398a66ad221317
Łukasz Mrozek <morskk@gmail.com>
2022-03-14 22:56:11 +0100

    Eight day of AoC 2021, part two

--- 2021/8/Program.fs
+++ 2021/8/Program.fs
@@ -8,6 +8,36 @@ type Signal =
     | F
     | G
 
+type Segment =
+    | T = 0
+    | TL = 1
+    | BL = 2
+    | B = 3
+    | TR = 4
+    | BR = 5
+    | M = 6
+
+[<System.Flags>]
+type Segments =
+    | None = 0
+    | T = 1
+    | TL = 2
+    | BL = 4
+    | B = 8
+    | TR = 16
+    | BR = 32
+    | M = 64
+    | Zero = 63
+    | One = 48
+    | Two = 93
+    | Three = 121
+    | Four = 114
+    | Five = 107
+    | Six = 111
+    | Seven = 49
+    | Eight = 127
+    | Nine = 123
+
 type SignalSet = Set<Signal>
 
 type Entry =
@@ -57,4 +87,92 @@ let firstPart (entries: seq<Entry>) =
         |> Seq.length)
     |> Seq.sum
 
-printfn "%d" (firstPart input)
+let secondPart entries =
+    let permutations array =
+        let swap (a: 'a []) x y =
+            let ax = a[x]
+            a[x] <- a[y]
+            a[y] <- ax
+
+        // Adapted 'procedure generate' from https://en.wikipedia.org/wiki/Heap%27s_algorithm
+        let rec permutations' k array =
+            seq {
+                if k = 1 then
+                    yield (Array.copy array)
+                else
+                    yield! permutations' (k - 1) array
+
+                    for i in 0 .. (k - 2) do
+                        if k % 2 = 0 then
+                            swap array i (k - 1)
+                        else
+                            swap array 0 (k - 1)
+
+                        yield! permutations' (k - 1) array
+            }
+
+        permutations' (Array.length array) (Array.copy array)
+
+    let toSegment (signals: Signal []) signal : Segment =
+        let index = Array.findIndex (fun x -> x = signal) signals
+        LanguagePrimitives.EnumOfValue(index)
+
+    let toSegmentFlags (segment: Segment) : Segments =
+        let shift = LanguagePrimitives.EnumToValue segment
+        LanguagePrimitives.EnumOfValue(1 <<< shift)
+
+    let toSegments (toSegment: Signal -> Segment) (signals: SignalSet) =
+        signals
+        |> Seq.map toSegment
+        |> Seq.fold (fun a b -> a ||| (toSegmentFlags b)) Segments.None
+
+    let toDigit segments =
+        match segments with
+        | Segments.Zero -> 0
+        | Segments.One -> 1
+        | Segments.Two -> 2
+        | Segments.Three -> 3
+        | Segments.Four -> 4
+        | Segments.Five -> 5
+        | Segments.Six -> 6
+        | Segments.Seven -> 7
+        | Segments.Eight -> 8
+        | Segments.Nine -> 9
+        | _ -> raise (System.InvalidOperationException(sprintf "Segments %O do not represent a digit" segments))
+
+    let toNumber digits =
+        Seq.foldBack (fun digit (multiplier, number) -> (multiplier * 10, number + (digit * multiplier))) digits (1, 0)
+        |> snd
+
+    let signals = [| A; B; C; D; E; F; G |]
+
+    let allToSegments =
+        permutations signals
+        |> Seq.map (toSegment >> toSegments)
+        |> Seq.toArray
+
+    let digits =
+        Set.ofArray [| Segments.Zero
+                       Segments.One
+                       Segments.Two
+                       Segments.Three
+                       Segments.Four
+                       Segments.Five
+                       Segments.Six
+                       Segments.Seven
+                       Segments.Eight
+                       Segments.Nine |]
+
+    entries
+    |> Seq.map (fun entry ->
+        let validToSegment =
+            allToSegments
+            |> Seq.filter (fun x -> (entry.patterns |> Seq.map x |> Set.ofSeq) = digits)
+            |> Seq.tryExactlyOne
+
+        match validToSegment with
+        | Some a -> entry.output |> Seq.map (a >> toDigit) |> toNumber
+        | None -> raise (System.InvalidOperationException(sprintf "Failed to resolve entry %O" entry)))
+    |> Seq.sum
+
+printfn "%d" (secondPart input)
